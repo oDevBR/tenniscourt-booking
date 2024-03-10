6ab13c3bd8191fd83e4ded6534da2691102aa7c6
oDevBR <joao.aquino.dev@gmail.com>
2024-03-10 15:46:16 -0300

    Fix eslint compatibility issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+
 import { classMerge } from '@/lib/utils';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
